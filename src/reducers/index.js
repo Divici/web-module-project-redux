bc4f3bc4c125e21f995961fd63455d2f9d1137a2
david-aihe <doa9200@gmail.com>
2022-01-18 17:34:20 -0500

    Connected displayFavorites

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,11 @@ import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
 
-export default movieReducer;
+import favoritesReducer from './favoritesReducer';
+
+const Reducers = combineReducers({
+    movieReducer, 
+    favoritesReducer
+})
+
+export default Reducers;
